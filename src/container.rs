3480b4566ce4e88bf3141742f46038a56c0446f2
Nathan Cox <ncox@covermymeds.com>
2023-03-22 14:52:59 -0700

    Extract output to status structs, use From trait
    Squashed commit of the following:
    
    commit 1f7d8cbfba6b00d9afdd23b8fc9555baedad299c
    Author: Nathan Cox <ncox@covermymeds.com>
    Date:   Wed Mar 22 14:48:27 2023 -0700
    
        Create status structs for output with From impl
    
    commit 671597b419e40bc946ff76c5bd963dd21360582c
    Author: Nathan Cox <ncox@covermymeds.com>
    Date:   Wed Mar 22 10:26:05 2023 -0700
    
        Fromify EOLDate
    
    commit de8239bf6d1e14927c4ad298b8c26e70f1109f69
    Author: Nathan Cox <ncox@covermymeds.com>
    Date:   Wed Mar 22 09:36:26 2023 -0700
    
        Move eol query method to struct, fromify Cycle
    
    commit 066d9e0b057c1a594be9338624db1ae38c28176e
    Author: Nathan Cox <ncox@covermymeds.com>
    Date:   Wed Mar 22 08:57:34 2023 -0700
    
        Drop println in test
    
    commit 55568d3986449fbe7bb666ee8031cd2e9b255591
    Author: Nathan Cox <ncox@covermymeds.com>
    Date:   Tue Mar 21 18:46:27 2023 -0700
    
        Extract docker execute into new module
    
    commit af10742acc9f482159eea14aa88310699d72ea79
    Author: Nathan Cox <ncox@covermymeds.com>
    Date:   Tue Mar 21 15:25:55 2023 -0700
    
        Extract run from Config struct

--- src/container.rs
+++ src/container.rs
@@ -5,3 +5,35 @@ pub struct Container {
     pub path: String,
     pub apps: Vec<String>,
 }
+
+pub struct Status {
+    pub name: String,
+    pub apps: Vec<crate::application::Status>,
+}
+
+impl Status {
+    pub fn new(name: String) -> Self {
+        Self { name, apps: vec![] }
+    }
+}
+
+impl From<Status> for String {
+    fn from(value: Status) -> Self {
+        let mut output = vec![];
+        output.push(format!("\n---Container: {:-<35}", value.name));
+
+        for app in value.apps.iter() {
+            let eol_status: String = match &app.eol_status {
+                Some(x) => x.to_string(),
+                None => String::from(""),
+            };
+
+            output.push(format!(
+                "\t{: <15}{: <10} {}",
+                &app.name, &app.version, eol_status,
+            ));
+        }
+
+        output.join("\n")
+    }
+}
