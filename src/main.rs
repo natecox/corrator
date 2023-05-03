0b0a2cbfc838961ec217be3a153aa51a3dccb63d
Nate Cox <nate@natecox.dev>
2023-05-03 15:49:26 -0700

    Follow XDG Base Directories for config file
    https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
-use std::{env, process};
-
-use corrator::Config;
+extern crate xdg;
 
 use clap::Parser;
+use corrator::Config;
+use std::{env, process};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    #[arg(short, long, default_value = "~/.corrator.toml")]
+    #[arg(short, long, default_value_t = default_config_path())]
     config_path: String,
+}
+
+fn default_config_path() -> String {
+    match xdg::BaseDirectories::with_prefix("corrator") {
+        Ok(x) => String::from(x.get_config_file("corrator.toml").to_str().unwrap()),
+        _ => String::from("~/.corrator"),
+    }
 }
 
 fn main() {
