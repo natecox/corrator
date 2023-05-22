b2d837ce5fb6484ebb1ece0709fdc61aed683711
Nate Cox <nate@natecox.dev>
2023-05-22 15:39:29 -0700

    doc: add documentation to container module

--- src/container.rs
+++ src/container.rs
@@ -1,11 +1,23 @@
 use serde::{Deserialize, Serialize};
 
+/// Configuration details for a container
+///
+/// A representation of a corrator.toml config provided by a user.
 #[derive(Serialize, Deserialize, Debug)]
 pub struct Container {
+	/// A full path to a docker image
 	pub path: String,
+
+	/// A list of the apps to query, by name
 	pub apps: Vec<String>,
 }
 
+/// The currency status of a container
+///
+/// Contains a list of [`application::Status`] providing currency details
+/// for each defined application in a container.
+///
+/// [`application::Status`]: ../application/Struct.status.html
 #[derive(Serialize)]
 pub struct Status {
 	pub name: String,
@@ -13,16 +25,42 @@ pub struct Status {
 }
 
 impl Status {
+	/// Create a new Status instance from a [`String`]
+	///
+	/// # Example
+	/// ```rust
+	/// corrator::container::Status::new(String::from("corrator"));
+	/// ```
 	pub fn new(name: String) -> Self {
 		Self { name, apps: vec![] }
 	}
 
+	/// Serialize a Status instance as JSON data
+	///
+	/// # Example
+	/// ```rust
+	/// let name = String::from("corrator");
+	/// let status = corrator::container::Status::new(name);
+	/// status.to_json();
+	/// ```
 	pub fn to_json(&self) -> String {
 		serde_json::to_string(&self).unwrap()
 	}
 }
 
 impl From<Status> for String {
+	/// A human readable representation of a container's currency
+	///
+	/// Includes version and end-of-life details for each configured
+	///	app inside a container.
+	///
+	/// # Example
+	///
+	/// ---Container: ubuntu-----------------------------
+	///		bash           5.1.16     
+	///		grep           3.7        
+	///		ubuntu         22.04      
+
 	fn from(value: Status) -> Self {
 		let mut output = vec![];
 		output.push(format!("\n---Container: {:-<35}", value.name));
