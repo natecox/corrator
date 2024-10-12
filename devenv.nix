7f3f5a96b6f47701986dab209dc40cd55b385c61
Nate Cox <nate@natecox.dev>
2024-10-11 21:38:00 -0700

    chore: switch from flake.nix to devenv

---
+++ devenv.nix
@@ -0,0 +1,56 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+  # https://devenv.sh/basics/
+  # env.GREET = "devenv";
+
+  # https://devenv.sh/packages/
+  packages = with pkgs; [
+    rust-analyzer
+    just
+    taplo
+    openssl
+  ];
+
+  # https://devenv.sh/languages/
+  languages.rust.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  # scripts.hello.exec = ''
+  #   echo hello from $GREET
+  # '';
+
+  # enterShell = ''
+  #   hello
+  #   git --version
+  # '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    cargo -v
+  '';
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+
+  # See full reference at https://devenv.sh/reference/options/
+}
